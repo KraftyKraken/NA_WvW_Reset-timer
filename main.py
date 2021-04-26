f9594e993b75fd1ff8bf79ea0c4f16ea1ada84f9
KraftyKraken <82181511+KraftyKraken@users.noreply.github.com>
2021-04-26 10:19:01 -0500

    Timing, eu, threading, and closing
    Fixed timing so it never can get paused, only reset. It's MUCH more accurate now.
    Added in EU timer for you chaps across the pond. Or however it is you describe it. Je veux dire, je ne sais pas.
    Added in threading and tried my really bestest to make it safe so I'm not a necromancer conjuring zombies.
    Added toggles for a simple windows ping sound when you hit 5 and/or 30 minutes left 
    Fixed a stuck 1 sec left display issue.
    Lets it close with grace now.
    Made a ping test to make sure it works.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# 6th draft of reset countdown timer.
+# 7th draft of reset countdown timer.
 # By: KraftyKraken - octonink@gmail.com
 # Created: 4/7/2021
-import _tkinter
 
+import _tkinter
 import dateutil.relativedelta as rel
 import datetime
+import winsound
+import time
+import threading
 from tkinter import *
 from tkinter import messagebox
 
@@ -19,14 +22,20 @@ except _tkinter.TclError:
     messagebox.showerror("Icon Error", "Unable to find icon resource. Put me back with my friends!")
     exit(1)
 
-# def time vars + other
+# Vars that need to be shared around to work.
 hour = StringVar()
 hour.set("0")
 minute = StringVar()
 minute.set("0")
 second = StringVar()
 second.set("0")
-running = True
+thirty_min = BooleanVar()
+thirty_min.set(False)
+five_min = BooleanVar()
+five_min.set(False)
+running = BooleanVar()
+running.set(True)
+temp = 0
 
 # create the boxes.
 hourEntry = Entry(root, width=4, font=("Arial", 14, ""), textvariable=hour)
@@ -38,11 +47,7 @@ secondEntry.place(x=133, y=20)
 
 
 def na_time():
-    # finds all the time calculations for na
-    # restart if we're stopped.
-    global running
-    running = True
-
+    running.set(True)
     today_utc = datetime.datetime.utcnow()
 
     # if today is not sat (sat 2am UTC when reset is)
@@ -64,12 +69,33 @@ def na_time():
             countdown(int((reset-today_utc).total_seconds()))
 
 
+def eu_time():
+    running.set(True)
+    today_utc = datetime.datetime.utcnow()
+
+    # if today is not fri (fri 6pm UTC when reset is)
+    # find next fri and say how long it is
+    if today_utc.weekday() != 4:
+        reset_utc = rel.relativedelta(hour=18, minute=0, second=0, microsecond=0, days=1, weekday=rel.FR)
+        reset = today_utc + reset_utc
+        countdown(int((reset-today_utc).total_seconds()))
+    # today is fri
+    else:
+        # we're after 6pm in the day just go about it normally
+        if datetime.datetime.utcnow().time() >= datetime.time(18, 0, 0, 0):
+            reset_utc = rel.relativedelta(hour=18, minute=0, second=0, microsecond=0, days=1, weekday=rel.FR)
+            reset = today_utc + reset_utc
+            countdown(int((reset-today_utc).total_seconds()))
+        # otherwise don't go to any other day, just give the 18hr countdown
+        else:
+            reset = today_utc + rel.relativedelta(hour=18, minute=0, second=0, microsecond=0)
+            countdown(int((reset-today_utc).total_seconds()))
+
+
+# finds military time only!
 def my_raid():
-    # finds custom time of today, set military time style
-    # restart if we're stopped.
-    global running
-    running = True
-    # Try to test cast all values to int
+    running.set(True)
+
     try:
         int(hour.get())
         int(minute.get())
@@ -80,7 +106,7 @@ def my_raid():
     now = datetime.datetime.today()
     try:
         target_time = rel.relativedelta(hour=int(hour.get()), minute=int(minute.get()),
-                                        second=int(minute.get()), microsecond=0)
+                                        second=int(second.get()), microsecond=0)
         my_time = now + target_time
         if int((my_time-now).total_seconds()) <= 0:
             messagebox.showerror("Bad Input", "Please use military time\nFor PM, simply add 12 hour.")
@@ -90,11 +116,20 @@ def my_raid():
                                           "minutes and seconds.")
 
 
+def accurate_time():
+    global temp
+    while temp > -1:
+        time.sleep(1)
+        temp -= 1
+
+
 def countdown(work_time):
-    # counts down the actual work
-    temp = 0
+    global temp
+    t1 = threading.Thread(target=lambda: accurate_time())
+
     try:
         temp = work_time
+        t1.start()
     except ValueError:
         messagebox.showerror("Value Error", "Incorrect values! Date Calculation malfunction")
     while temp > -1:
@@ -112,55 +147,61 @@ def countdown(work_time):
         minute.set("{0:2d}".format(minutes))
         second.set("{0:2d}".format(seconds))
 
-        # Try to update output and countdown.
-        try:
-            if running:
+        # we should auto-join here because acc_time can't run, so it joins default below
+        if not running.get():
+            temp = -1
+        else:
+            try:
+                if thirty_min.get() and temp == 1800:
+                    winsound.PlaySound("SystemExclamation", winsound.SND_ASYNC)
+                elif five_min.get() and temp == 300:
+                    winsound.PlaySound("SystemExclamation", winsound.SND_ASYNC)
                 root.update()
                 root.after(1000)
-                temp -= 1
-            else:
+            except _tkinter.TclError:
                 temp = -1
-                hour.set("0")
-                minute.set("0")
-                second.set("0")
-                root.update()
-        except _tkinter.TclError:
-            return
+                t1.join()
+                return
+    # setting helps fix stuck 1 left issue as well as go back to default if not running
+    hour.set("0")
+    minute.set("0")
+    second.set("0")
+    root.update()
+    t1.join()
 
 
+# nastier than before, but safer to delete the thread
 def close_program():
     response = messagebox.askyesno("Reset Countdown - Close?", "Do you want to close the program?")
-    if response == 1:
-        root.destroy()
-    else:
-        reply = messagebox.askokcancel("Reset Countdown - Accuracy", "Program was paused. "
-                                                                     "Timing is now off. Press ok "
-                                                                     "to re-enter desired time. Cancel to continue.")
-        if reply == 1:
-            stop_program()
-
-
-def stop_program():
-    global running
-    running = False
+    if response:
+        global temp
+        temp = -1
+        root.quit()
 
 
 btn = Button(root, text=" NA ", bd='5', command=na_time)
 btn.place(x=20, y=60)
+btn = Button(root, text=" EU ", bd='5', command=eu_time)
+btn.place(x=65, y=60)
 btn = Button(root, text=" Custom Today ", bd='5', command=my_raid)
 btn.place(x=20, y=95)
 
 # menu bar
 menu_bar = Menu(root)
 file_menu = Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Reset", command=stop_program)
+file_menu.add_command(label="Reset", command=lambda: running.set(False))
+file_menu.add_separator()
+file_menu.add_checkbutton(label="5 Minute Reminder Ping", command=lambda: five_min.set(not five_min.get()))
+file_menu.add_checkbutton(label="30 Minute Reminder Ping", command=lambda: thirty_min.set(not thirty_min.get()))
 file_menu.add_separator()
 file_menu.add_command(label="Close", command=close_program)
 menu_bar.add_cascade(label="File", menu=file_menu)
 
 # help menu
 help_menu = Menu(menu_bar, tearoff=0)
+help_menu.add_command(label="Ping Test", command=lambda: winsound.PlaySound("SystemExclamation", winsound.SND_ASYNC))
 menu_bar.add_cascade(label="Help", menu=help_menu)
 
 root.config(menu=menu_bar)
+root.protocol("WM_DELETE_WINDOW", close_program)
 root.mainloop()
